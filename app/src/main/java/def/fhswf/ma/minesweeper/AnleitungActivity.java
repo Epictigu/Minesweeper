3bbb990f9879964c2b899af1324da4bdb970ebf7
Borgreth <nolzen.marcus@gmail.com>
2022-01-10 20:00:08 +0100

    version Hardcoded Hilfe Text

--- app/src/main/java/def/fhswf/ma/minesweeper/AnleitungActivity.java
+++ app/src/main/java/def/fhswf/ma/minesweeper/AnleitungActivity.java
@@ -2,9 +2,6 @@ package def.fhswf.ma.minesweeper;
 
 import android.os.Bundle;
 
-import com.github.barteksc.pdfviewer.PDFView;
-import com.github.barteksc.pdfviewer.listener.OnRenderListener;
-import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -16,19 +13,6 @@ public class AnleitungActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anleitung);
-        PDFView pdfView = findViewById(R.id.pdfView);
-
-        pdfView.fitToWidth(1);
-        pdfView.fromAsset("Hilfe.pdf")
-                .scrollHandle(new DefaultScrollHandle(this))
-                .spacing(0)
-                .onRender(new OnRenderListener() {
-                    @Override
-                    public void onInitiallyRendered(int pages, float pageWidth, float pageHeight) {
-                        pdfView.fitToWidth(1);
-                    }
-                })
-                .load();
 
         Button exitButton = (Button) findViewById(R.id.exitButton);
         exitButton.setOnClickListener(new View.OnClickListener() {
